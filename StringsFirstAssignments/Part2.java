3a56d92b6bba8cb052a7db3f031c8f31c8eef568
Mario Loera <marioll@kth.se>
2021-01-16 13:50:03 +0100

    findSimpleGene for upper and lower case

--- StringsFirstAssignments/Part2.java
+++ StringsFirstAssignments/Part2.java
@@ -9,24 +9,38 @@ import java.util.HashMap;
  */
 public class Part2 {
 
-    public String findSimpleGene(String dna, String startCodon, String stopCodon){
-        /*
-        Write the method findSimpleGene that has one String parameter dna,
-        representing a string of DNA.
+    public String findSimpleGene(String dnaI, String startCodonI, String stopCodonI){
+        /** doc string in java
+            Write the method findSimpleGene that has one String parameter dna,
+            representing a string of DNA.
 
-        This method does the following:
+            This method does the following:
 
-        1 - Finds the index position of the start codon “ATG”.
-            If there is no “ATG”, return the empty string.
+            1 - Finds the index position of the start codon “ATG”.
+                If there is no “ATG”, return the empty string.
 
-        2 - Finds the index position of the first stop codon “TAA”
-            appearing after the “ATG” that was found.
-            If there is no such “TAA”, return the empty string.
+            2 - Finds the index position of the first stop codon “TAA”
+                appearing after the “ATG” that was found.
+                If there is no such “TAA”, return the empty string.
 
-        3 - If the length of the substring between the “ATG” and “TAA”
-            is a multiple of 3, then return the substring that starts with
-            that “ATG” and ends with that “TAA”.
+            3 - If the length of the substring between the “ATG” and “TAA”
+                is a multiple of 3, then return the substring that starts with
+                that “ATG” and ends with that “TAA”.
+
+            4 - 4. Modify the findSimpleGene method to work with DNA strings
+                that are either all uppercase letters such as “ATGGGTTAAGTC” 
+                or all lowercase letters such as “gatgctataat”.
+                Calling findSimpleGene with “ATGGGTTAAGTC” should return the answer
+                with uppercase letters, the gene “ATGGGTTAA”, and calling
+                findSimpleGene with  “gatgctataat” should return the answer with
+                lowercase letters, the gene “atgctataa”.
+                HINT: there are two string methods toUpperCase() and toLowerCase().
+                If dna is the string “ATGTAA” then dna.toLowerCase() results in the
+                string “atgtaa”.
         */
+        String dna = dnaI.toUpperCase();
+        String startCodon = startCodonI.toUpperCase();
+        String stopCodon = stopCodonI.toUpperCase();
 
         int startIndex = dna.indexOf(startCodon);
         if (startIndex == -1){
@@ -42,17 +56,23 @@ public class Part2 {
             return "";
         }
 
-        String gen = dna.substring(startIndex, stopIndex);
+        String gen = dnaI.substring(startIndex, stopIndex);
         return gen;
     }
 
     public void testFindSimpleGene(){
+        /**
+            doc string in java
+        */
         System.out.println("Start testFindSimpleGene  ");
         HashMap<String, String> examples = new HashMap<String, String>();
 
         examples.put("ATGAAATAA", "ATGAAATAA");
         examples.put("ATGAAATAAz", "ATGAAATAA");
         examples.put("ATGAAAGGGTAA", "ATGAAAGGGTAA");
+        examples.put("atgaaataa", "atgaaataa");
+        examples.put("atgaaataaz", "atgaaataa");
+        examples.put("atgaaagggtaa", "atgaaagggtaa");
         examples.put("ATGAATAA", "");
         examples.put("TTGAAATAA", "");
         examples.put("ATGAAATGA", "");
