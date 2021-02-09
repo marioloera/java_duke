20b9e762c812a6f8afe534f41d5f12583e9767c5
Mario Loera <marioll@kth.se>
2021-02-10 00:47:45 +0100

    re-use listExporters

--- Csv/exports/WhichCountriesExport.java
+++ Csv/exports/WhichCountriesExport.java
@@ -9,17 +9,7 @@ import java.util.*;
 
 public class WhichCountriesExport {
     public void listExporters(CSVParser parser, String exportOfInterest) {
-        //for each row in the CSV File
-        for (CSVRecord record : parser) {
-            //Look at the "Exports" column
-            String export = record.get("Exports");
-            //Check if it contains exportOfInterest
-            if (export.contains(exportOfInterest)) {
-                //If so, write down the "Country" from that row
-                String country = record.get("Country");
-                System.out.println(country);
-            }
-        }
+        listExporters(parser, exportOfInterest, exportOfInterest);
     }
 
     public void listExporters(CSVParser parser, String exportItem1, String exportItem2) {
@@ -66,7 +56,7 @@ public class WhichCountriesExport {
         FileResource fr = new FileResource();
         CSVParser parser;
         parser = fr.getCSVParser();
-        System.out.println("whoExports dold & diamonds");
+        System.out.println("whoExports gold & diamonds");
         listExporters(parser, "gold", "diamonds");
         parser = fr.getCSVParser();
         System.out.println("whoExports coffee & vanilla");
